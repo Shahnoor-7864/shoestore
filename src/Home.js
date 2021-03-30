fd2e4884e191954959f663b50d2a8604180527f5
shahnoor-7864 <shaninad32@gmail.com>
2021-03-29 20:48:39 -0700

    my changes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Home  () {
 return (
-<h1>welcome to Home </h1> );
+    <>
+<h1 >welcome to Home </h1> 
+<Link to ="/Product">Welcome To My Shoe Store</Link>
+</>
+);
 }
 export default Home;
